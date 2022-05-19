0f8abf035ba5474afce028c45371dca657e1b7f2
anan416 <anacc11104@gmail.com>
2022-05-19 17:24:51 -0300

    principio

---
+++ Javascript/cod.js
@@ -0,0 +1,44 @@
+var imagen = document.querySelector(".cambio");
+
+function abrir1(){
+    if (document.querySelector(".contenido1").style.display == "none"){
+        document.querySelector(".contenido1").style.display = "inline";
+        document.querySelector(".contenido1").style.height = "190px";
+    }
+    else{
+        document.querySelector(".contenido1").style.display = "none";
+        document.querySelector(".contenido1").style.height = "0px";
+    }
+}
+
+function abrir0(){
+    if (document.querySelector(".contenido0").style.display == "none"){
+        document.querySelector(".contenido0").style.display = "inline";
+        document.querySelector(".contenido0").style.height = "190px";
+    }
+    else{
+        document.querySelector(".contenido0").style.display = "none";
+        document.querySelector(".contenido0").style.height = "0px";
+    }
+}
+
+function responsivo(){
+    if (document.querySelector(".responsive").style.display == "block"){
+        document.querySelector(".responsive").style.display = "none";
+    }
+    else{
+        document.querySelector(".responsive").style.display = "block";
+    }
+}
+
+function imagen1(){
+    imagen.src = "../Imagenes/saconaturalm1.jpeg";
+}
+
+function imagen2(){
+    imagen.src = "../Imagenes/saconaturalm2.jpeg";
+}
+
+function imagen3(){
+    imagen.src = "../Imagenes/saconaturalm3.jpeg";
+}
